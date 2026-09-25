c9ee107f54353e7777f7ffa6938758b7e842665a
lukamlnvc11311 <145862627+lukamlnvc11311@users.noreply.github.com>
2026-09-25 19:03:25 +0200

    Main-Klasse zur Verknüpfung von MVC fertiggestellt

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
   public static void main(String[] args) {
+    GewinnModel model = new GewinnModel();
+    GewinnView view = new GewinnView();
+    new GewinnController(model, view);
+    view.setVisible(true);
   }
 }
