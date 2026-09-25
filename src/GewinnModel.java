8753552f0f3d0178df4709cf1c9fddc02f598ac8
lukamlnvc11311 <145862627+lukamlnvc11311@users.noreply.github.com>
2026-09-25 18:00:09 +0200

    Methoden berechneComputerZahl und berechneRunde

--- src/GewinnModel.java
+++ src/GewinnModel.java
@@ -23,4 +23,24 @@ public class GewinnModel {
     public int getRundenErgebnis() {
         return rundenErgebnis;
     }
+
+    public void berechneComputerZahl() {
+        this.computerZahl = random.nextInt(9) + 1;
+    }
+
+    public void berechneRunde(int spielerZahl) {
+        this.spielerZahl = spielerZahl;
+        berechneComputerZahl();
+
+        int diff = Math.abs(this.spielerZahl - this.computerZahl);
+        if (diff == 0) {
+            this.rundenErgebnis = 20;
+        } else if (diff == 1) {
+            this.rundenErgebnis = 5;
+        } else {
+            this.rundenErgebnis = -10;
+        }
+
+        this.gesamtPunkte += this.rundenErgebnis;
+    }
 }
